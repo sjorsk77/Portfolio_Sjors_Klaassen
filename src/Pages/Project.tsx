c73945517fd979069ec8be5a225f0918f39f9fcc
sjorsklaassen <sjors.klaassen@gmail.com>
2025-02-25 14:12:47 +0100

    added working on it to project page

--- src/Pages/Project.tsx
+++ src/Pages/Project.tsx
@@ -7,6 +7,7 @@ import {FaFlagCheckered} from "react-icons/fa";
 import {MdOutlineNotStarted} from "react-icons/md";
 import {PageContainer} from "../Pages/PageContainer";
 import {ProjectDescription} from "../Components/Project/ProjectDescription";
+import {WorkingOnIt} from "../Components/WorkingOnIt";
 
 
 export const Project: React.FC = () => {
@@ -23,22 +24,7 @@ export const Project: React.FC = () => {
 
     return (
         <PageContainer>
-            <ProjectHeader title={project.title}/>
-            <ProjectDescription description={project.description}/>
-            <div className="h-full">
-
-
-            <Timeline active={2} bulletSize={64} lineWidth={6} style={{ flex: 1}}>
-                <Timeline.Item bullet={<MdOutlineNotStarted size={32}/>} title="Project start">
-                    <Text size="xs" mt={4}>{project.startDate}</Text>
-                </Timeline.Item>
-                <Timeline.Item bullet={<FaFlagCheckered size={32}/>} title="Project finished" style={{ flex: 1 }}>
-                    <Text size="xs" mt={4}>{project.endDate}</Text>
-                </Timeline.Item>
-            </Timeline>
-            </div>
-
-
+            <WorkingOnIt/>
         </PageContainer>
     );
 };
